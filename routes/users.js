459c1d8a2ba71782ab12a391780bdda0f53a9cc8
It's MD <90133844+Mohammadhussain-Ambaliyasana@users.noreply.github.com>
2024-12-26 18:18:45 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.post('/create', (req, res) => {
     try {
-        res.send("Hello");
+        res.send("Hello World");
     } catch (error) {
         return res.status(500).json({"message":"Internal Server Error", "success": false});
     }
